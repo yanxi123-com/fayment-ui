d46e6c380988877ef842fbf54aacb1e21d07d96b
yanxi <blogbbs@gmail.com>
2020-04-12 11:03:50 +0800

    update http rpc endpoint

--- src/lib/eos.ts
+++ src/lib/eos.ts
@@ -1,6 +1,8 @@
 import { JsonRpc } from "eosjs";
 
-const rpc = new JsonRpc("https://mainnet.eos.dfuse.io");
+// https://mainnet.eos.dfuse.io
+// https://api1.eosasia.one
+const rpc = new JsonRpc("https://api1.eosasia.one");
 
 export async function getAccountInfo(name: string) {
   return rpc.get_account(name);
