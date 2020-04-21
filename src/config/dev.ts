276fb0041a69a67372c4f5aeb7c321b5fc5f6bc8
yanxi <blogbbs@gmail.com>
2020-04-22 01:17:59 +0800

    update aws lambda url

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -1,4 +1,4 @@
 export const apiGateway =
-  "https://jqjh6by9td.execute-api.ap-northeast-1.amazonaws.com/dev";
+  "https://jqjh6by9td.execute-api.ap-northeast-1.amazonaws.com/fayment";
 
 export const rpcService = "http://localhost:9061";
--- src/config/prod.ts
+++ src/config/prod.ts
@@ -1,4 +1,4 @@
 export const apiGateway =
-  "https://jqjh6by9td.execute-api.ap-northeast-1.amazonaws.com/";
+  "https://jqjh6by9td.execute-api.ap-northeast-1.amazonaws.com/fayment";
 
 export const rpcService = "http://49.233.73.172:8010";
