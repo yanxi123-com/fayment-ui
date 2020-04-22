1fd4200ce8d65927d431fae1f70aae5460caffb0
yanxi <blogbbs@gmail.com>
2020-04-22 13:13:59 +0800

    update api url

--- src/config/prod.ts
+++ src/config/prod.ts
@@ -1,4 +1,4 @@
 export const apiGateway =
   "https://jqjh6by9td.execute-api.ap-northeast-1.amazonaws.com/fayment";
 
-export const rpcService = "http://49.233.73.172:8010";
+export const rpcService = "https://api.fayment.com";
